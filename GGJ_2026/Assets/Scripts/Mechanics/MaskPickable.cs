a35a81b83958279ce6128918e929b7f6aac8f6f2
lentilek <laura.robinska@wp.pl>
2026-01-31 18:55:21 +0100

    Pickables in forest

--- GGJ_2026/Assets/Scripts/Mechanics/MaskPickable.cs
+++ GGJ_2026/Assets/Scripts/Mechanics/MaskPickable.cs
@@ -5,7 +5,46 @@ using UnityEngine;
 public class MaskPickable : MonoBehaviour
 {
     public int maskNumber; // 1 - forest, 2 - cementary, 3 - circus
+    [SerializeField] private GameObject child;
 
+    private void Start()
+    {
+        switch(maskNumber)
+        {
+            case 1:
+                if(PlayerControler.Instance.ps.forestUnlocked)
+                {
+                    Destroy(gameObject);
+                }
+                else
+                {
+                    child.SetActive(true);
+                }
+                break;
+            case 2:
+                if (PlayerControler.Instance.ps.cementaryUnlocked)
+                {
+                    Destroy(gameObject);
+                }
+                else
+                {
+                    child.SetActive(true);
+                }
+                break;
+            case 3:
+                if(PlayerControler.Instance.ps.circusUnlocked)
+                {
+                    Destroy(gameObject);
+                }
+                else
+                {
+                    child.SetActive(true);
+                }
+                break;
+            default:
+                break;
+        }
+    }
     private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.tag == "Player")
